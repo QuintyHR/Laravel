c5274c150b8e9c62da453495f9bb7e1b11fe1ee6
QuintyHR <70649864+QuintyHR@users.noreply.github.com>
2021-09-13 16:59:29 +0200

    Followed the second programming lesson and cleared up the html for the about page

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -8,6 +8,19 @@ class AboutController extends Controller
 {
     public function index()
     {
-        return view('about');
+        $title = "Over onze website";
+        $paragraph = "Hello world!";
+        $musicAlbums = [
+            [
+                'artist' => '1',
+                'title' => 'Album 1'
+            ],
+            [
+                'artist' => '2',
+                'title' => 'Album 2'
+            ]
+        ];
+
+        return view('about', compact('title', 'paragraph', 'musicAlbums'));
     }
 }
