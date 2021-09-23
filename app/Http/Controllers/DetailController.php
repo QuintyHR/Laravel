704ddc365da8a05093c796b7d40565f1d7c7ee75
QuintyHR <70649864+QuintyHR@users.noreply.github.com>
2021-09-23 14:35:27 +0200

    Added the Detail controller and model

---
+++ app/Http/Controllers/DetailController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Character;
+use Illuminate\Http\Request;
+
+class DetailController extends Controller
+{
+    public function index()
+    {
+        //$title = "";
+
+        //return view('characters.detail', compact('title', 'characters'));
+    }
+}
