2923987acff74cc7949eb124b12e8c47d08e089c
QuintyHR <70649864+QuintyHR@users.noreply.github.com>
2021-09-23 14:26:04 +0200

    Changed the character table to have an image column and added the character_user table

--- database/migrations/2021_09_23_113654_create_character_user_table.php
+++ database/migrations/2021_09_23_113654_create_character_user_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AlterCharactersTable extends Migration
+class CreateCharacterUserTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,10 @@ class AlterCharactersTable extends Migration
      */
     public function up()
     {
-        Schema::table('characters', function (Blueprint $table) {
-            $table->text('image')->after('description')->nullable();
+        Schema::create('character_user', function (Blueprint $table) {
+            $table->bigInteger('user_id');
+            $table->bigInteger('character_id');
+            $table->timestamps();
         });
     }
 
@@ -25,8 +27,6 @@ class AlterCharactersTable extends Migration
      */
     public function down()
     {
-        Schema::table('characters', function (Blueprint $table) {
-            $table->dropColumn('image');
-        });
+        Schema::dropIfExists('character_user');
     }
 }
