e008ec0fc70dc2d2807b50dcd6d90ad615486d6e
QuintyHR <70649864+QuintyHR@users.noreply.github.com>
2021-09-27 11:13:09 +0200

    Made a little tweak on the home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,4 @@
 <x-layout>
-    <section>
-        <h1>League of Legends Character Collection Wiki</h1>
-        <p>Welcome to the League of Legends Character Collection Wiki! Here you can create tabs for your League of Legends Characters and help me out to complete the collection.</p>
-    </section>
+    <h1>League of Legends Character Collection Wiki</h1>
+    <p>Welcome to the League of Legends Character Collection Wiki! Here you can create tabs for your League of Legends Characters and help me out to complete the collection.</p>
 </x-layout>
