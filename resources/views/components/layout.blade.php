d855711d0875771a57f8ac46bfd6ddd29005b20b
QuintyHR <70649864+QuintyHR@users.noreply.github.com>
2021-10-03 11:05:00 +0200

    Changed the nav bar so it's different when you're logged in or are a guest

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -10,19 +10,44 @@
 
 </head>
     <body>
-
         <nav>
-            <div class="nav-left">
-                <div class="nav"><a href="/">Home</a></div>
-                <div class="nav"><a href="/characters">Character Collection</a></div>
-                <div class="nav"><a href="/about">About</a></div>
-                <div class="nav"><a href="/profile">Profile</a></div>
-                <div class="nav"><a href="/create">Create Character</a></div>
-            </div>
-            <div class="nav-right">
-                <div class="nav"><a href="/login">Login</a></div>
-                <div class="nav"><a href="/register">Register</a></div>
-            </div>
+            @guest
+                <div class="nav-left">
+                    <div class="nav"><a href="/">Home</a></div>
+                    <div class="nav"><a href="/characters">Character Collection</a></div>
+                    <div class="nav"><a href="/about">About</a></div>
+                </div>
+                <div class="nav-right">
+                    @if (Route::has('login'))
+                        <div class="nav"><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></div>
+                    @endif
+
+                    @if (Route::has('register'))
+                        <div class="nav"><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></div>
+                    @endif
+                </div>
+            @else
+                <div class="nav-left">
+                    <div class="nav"><a href="/">Home</a></div>
+                    <div class="nav"><a href="/characters">Character Collection</a></div>
+                    <div class="nav"><a href="/about">About</a></div>
+                    <div class="nav"><a href="/profile">Profile</a></div>
+                    <div class="nav"><a href="/create">Create Character</a></div>
+                </div>
+                <div class="nav-right">
+                    <div>
+                        <a href="{{ route('logout') }}"
+                            onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </div>
+                </div>
+            @endguest
         </nav>
 
         <br>
