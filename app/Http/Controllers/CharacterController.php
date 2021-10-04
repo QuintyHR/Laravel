171b550953b14a340fbc555618d797ac33532f6b
QuintyHR <70649864+QuintyHR@users.noreply.github.com>
2021-10-04 17:43:15 +0200

    Made a form on the create page that fully works including images. Images are now also shown on the page when called for

---
+++ app/Http/Controllers/CharacterController.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Character;
+use Illuminate\Http\Request;
+
+class CharacterController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $title = "League of Legends Character Collection";
+
+        //SELECT * FROM characters
+        //dd($characters);
+
+        $search = $request->input('search');
+
+        if (!$search) {
+
+            $characters = Character::all()
+                ->where('active', '=', 1);
+        } else {
+            $characters = Character::where('name','like','%'.$search.'%')
+                ->where('active', '=', 1)
+                ->orderBy('id')
+                ->paginate(6);
+        }
+
+        return view('characters.index', compact('title', 'characters'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $title = 'Create new character';
+
+        return view('characters.create', compact('title'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $title = 'Create new character';
+
+        // Validate the inputs
+        $request->validate([
+            'name' => 'required',
+            'description' => 'required',
+            'creator' => 'required',
+            'tag' => 'required'
+        ]);
+
+        // ensure the request has a file before we attempt anything else.
+        if ($request->hasFile('file')) {
+
+            $request->validate([
+                'image' => 'mimes:jpeg,jpg,bmp,png' // Only allow .jpg, .bmp and .png file types.
+            ]);
+
+            // Save the file locally in the storage/public/ folder under a new folder named /product
+            $request->file->store('character', 'public');
+
+            // Store the record, using the new file hashname which will be it's new filename identity.
+            $character = new Character([
+                "name" => $request->get('name'),
+                "description" => $request->get('description'),
+                "creator" => $request->get('creator'),
+                "tag" => $request->get('tag'),
+                "image" => $request->file->hashName()
+            ]);
+            $character->save(); // Finally, save the record.
+        }
+
+        return view('characters.create', compact('title'));
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
