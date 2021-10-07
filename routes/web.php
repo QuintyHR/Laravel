8113cbb5887944ee1f4e256a8365afebcfc95435
QuintyHR <70649864+QuintyHR@users.noreply.github.com>
2021-10-07 14:42:02 +0200

    Added admin pages with validation that you need to have the admin role

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AboutController;
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\CharacterController;
 use App\Http\Controllers\CharacterOldController;
 use App\Http\Controllers\CreateController;
@@ -19,20 +20,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Home
 Route::get('/', function () {
     return view('welcome');
 });
 
+//Main pages
 Route::get('/about', [AboutController::class, 'index']);
 Route::get('/characters', [CharacterController::class, 'index']);
 Route::get('/detail/{character}', [DetailController::class, 'index']);
 
+//Accounts
 Route::get('/profile', [ProfileController::class, 'index'])->middleware('auth');
+Route::get('/admin', [AdminController::class, 'index'])->middleware('auth');
 
+//Character editing
 Route::resource('characters', CharacterController::class)->middleware('auth');
-
 Route::get('delete/{id}', [CharacterController::class, 'delete'])->middleware('auth');
 
+
+//Laravel login
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
